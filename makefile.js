a90dfbcc7ea52c3bd089a89ed35c2235c3de765a
Casey Visco <cvisco@gmail.com>
2015-08-08 18:55:44 -0500

    Build: Add checkRules task to makefile

--- makefile.js
+++ makefile.js
@@ -1,10 +1,12 @@
 "use strict";
 
+/* global cat */
 /* global echo */
 /* global exec */
 /* global exit */
 /* global find */
 /* global target */
+/* global test */
 
 //------------------------------------------------------------------------------
 // Requirements
@@ -12,6 +14,7 @@
 
 require("shelljs/make");
 
+var path = require("path");
 var nodeCLI = require("shelljs-nodecli");
 
 //------------------------------------------------------------------------------
@@ -66,10 +69,120 @@ target.lint = function () {
     }
 };
 
+/**
+ * Verify that each rule is defined in index.js with the correct default
+ * setting, has documentation and has tests.
+ */
+target.checkRules = function () {
+    echo("Verifying rules");
+
+    var errors = 0;
+
+    var ruleFiles = find("lib/rules/").filter(fileType("js"));
+    var configFile = require("./index");
+    var readmeText = cat("./README.md");
+
+    function isDefinedInConfig(rule) {
+        return configFile.rules.hasOwnProperty(rule);
+    }
+
+    function hasDefaultSettingInConfig(rule) {
+        var setting = configFile.rulesConfig[rule];
+
+        return typeof setting === "number" ||
+               (setting && typeof setting[0] === "number");
+    }
+
+    function isOffInConfig(rule) {
+        var setting = configFile.rulesConfig[rule];
+
+        return setting === 0 || (setting && setting[0] === 0);
+    }
+
+    function isOnInConfig(rule) {
+        return !isOffInConfig(rule);
+    }
+
+    function isOffInReadme(rule) {
+        var line = new RegExp("\\* \\[" + rule + "\\].*").exec(readmeText);
+
+        return (line ? line[0] : "").indexOf("(off by default)") !== -1;
+    }
+
+    function isOnInReadme(rule) {
+        return !isOffInReadme(rule);
+    }
+
+    function hasIdInTitle(docFile, id) {
+        var docText = cat(docFile);
+        var idInTitleRegExp = new RegExp("^# (.*?) \\(" + id + "\\)");
+
+        return idInTitleRegExp.test(docText);
+    }
+
+    ruleFiles.forEach(function (filename) {
+        var basename = path.basename(filename, ".js");
+        var docFilename = "docs/rules/" + basename + ".md";
+        var testFilename = "tests/lib/rules/" + basename + ".js";
+
+        // Verify rule has documentation
+
+        if (!test("-f", docFilename)) {
+            console.error("Missing documentation for rule %s", basename);
+            errors++;
+        } else {
+            if (readmeText.indexOf("(" + docFilename + ")") === -1) {
+                console.error("Missing link to documentation for rule %s in index", basename);
+                errors++;
+            }
+
+            // check for proper doc format
+            if (!hasIdInTitle(docFilename, basename)) {
+                console.error("Missing id in the doc page's title of rule %s", basename);
+                errors++;
+            }
+        }
+
+        // Verify rule has tests
+
+        if (!test("-f", testFilename)) {
+            console.error("Missing tests for rule %s", basename);
+            errors++;
+        }
+
+        // Verify config
+
+        if (!isDefinedInConfig(basename)) {
+            console.error("Missing rule entry for %s in index.js", basename);
+            errors++;
+        }
+
+        if (!hasDefaultSettingInConfig(basename)) {
+            console.error("Missing default setting for %s in index.js", basename);
+            errors++;
+        }
+
+        if (isOffInReadme(basename) && isOnInConfig(basename)) {
+            console.error("README states that %s is off by default, but it is enabled in index.js.", basename);
+            errors++;
+        }
+
+        if (isOffInConfig(basename) && isOnInReadme(basename)) {
+            console.error("Missing '(off by default)' for rule %s in README", basename);
+            errors++;
+        }
+    });
+
+    if (errors) {
+        exit(1);
+    }
+};
+
 target.test = function () {
     var errors = 0;
 
     target.lint();
+    target.checkRules();
 
     echo("Running test suite");
 
